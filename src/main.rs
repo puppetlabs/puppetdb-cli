274acf3921e0c6d5fb214504c9a5e49496ae5c1c
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-12-04 20:14:39 -0800

    (maint) Initial commit of rust puppetdb cli

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#[macro_use] extern crate hyper;
+extern crate docopt;
+extern crate serde;
+
+use hyper::Client;
+use hyper::header::{Connection,Headers};
+use docopt::Docopt;
+
+
+fn main() {
+    println!("Hello, world!");
+}
