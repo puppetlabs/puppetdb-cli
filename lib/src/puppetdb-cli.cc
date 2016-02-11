9ddf80946b68499890bd59fcadfd6a795583f97a
Andrew Roetker <andrew.roetker@puppetlabs.com>
2016-02-11 13:13:49 -0800

    (PDB-2385) Stream parse responses from PuppetDB
    This commit adds functionality to our CLI which will stream parse the
    JSON repsonses and errors from PuppetDB. This allows us to stream
    pretty-printed responses and eventually stream tabular responses to the
    user.

--- lib/src/puppetdb-cli.cc
+++ lib/src/puppetdb-cli.cc
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <string>
+#include <queue>
 
+#include <rapidjson/reader.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/filewritestream.h>
+
+
+#define BOOST_THREAD_PROVIDES_GENERIC_SHARED_MUTEX_ON_WIN
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/lock_types.hpp>
+#include <boost/thread/condition_variable.hpp>
+#include <boost/utility/string_ref.hpp>
+#include <boost/algorithm/string.hpp>
 #include <boost/nowide/iostream.hpp>
 #include <boost/nowide/args.hpp>
 #include <boost/filesystem.hpp>
@@ -25,7 +39,7 @@ namespace logging = leatherman::logging;
 string
 version()
 {
-    LOG_DEBUG("puppetdb-cli version is %1%", PUPPETDB_CLI_VERSION_WITH_COMMIT);
+    LOG_DEBUG("puppetdb-cli version is {1}", PUPPETDB_CLI_VERSION_WITH_COMMIT);
     return PUPPETDB_CLI_VERSION_WITH_COMMIT;
 }
 
@@ -107,13 +121,143 @@ PuppetDBConn::parseServerUrls(const json::JsonContainer& config) {
     }
 }
 
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+class SynchronizedCharQueue {
+  public:
+    char front() {
+        boost::unique_lock<boost::mutex> mlock(mutex_);
+        cond_.wait(mlock, [&]{ return !queue_.empty(); });
+        return queue_.front();
+    }
+
+    char pop() {
+        boost::unique_lock<boost::mutex> mlock(mutex_);
+        cond_.wait(mlock,  [&]{ return !queue_.empty(); });
+        auto item = queue_.front();
+        queue_.pop();
+        return item;
+    }
+
+    void push_range(vector<char>& items) {
+        boost::unique_lock<boost::mutex> mlock(mutex_);
+        for (auto item : items) queue_.push(item);
+        mlock.unlock();
+        cond_.notify_one();
+    }
+
+    void push(char item) {
+        boost::unique_lock<boost::mutex> mlock(mutex_);
+        queue_.push(item);
+        mlock.unlock();
+        cond_.notify_one();
+    }
+
+    int size() { return queue_.size(); }
+
+  private:
+    queue<char> queue_;
+    boost::mutex mutex_;
+    boost::condition_variable cond_;
+};
+
+struct UserData {
+    bool collecting_header;
+    vector<string> headers;
+    string content_type;
+    string error_response;
+    SynchronizedCharQueue stream;
+};
+
+size_t write_queue(char *ptr, size_t size, size_t nmemb, UserData& userdata) {
+    const size_t written = size * nmemb;
+    if (userdata.collecting_header) {
+        if (written == 2 && ptr[0] == '\r' && ptr[1] == '\n') {
+            // We've reached the end of the header
+            userdata.collecting_header = false;
+            return written;
+        } else {
+            boost::string_ref header(ptr, written);
+            userdata.headers.push_back(header.to_string());
+
+            if (!header.starts_with("HTTP/")) {
+                auto pos = header.find_first_of(':');
+                if (pos == boost::string_ref::npos) {
+                    LOG_WARNING("unexpected HTTP response header: {1}.", header);
+                } else {
+                    auto name = header.substr(0, pos).to_string();
+                    boost::trim(name);
+                    if (name == "Content-Type") {
+                        auto value = header.substr(pos + 1).to_string();
+                        boost::trim(value);
+                        userdata.content_type = value;
+                    }
+                }
+            }
+        }
+    } else {
+        vector<char> buffer(ptr, ptr + written);
+        userdata.stream.push_range(buffer);
+    }
+    return written;
+}
+
+size_t write_data(char *ptr, size_t size, size_t nmemb, FILE *stream) {
     const size_t written = fwrite(ptr, size, nmemb, stream);
     return written;
 }
 
 size_t write_body(char *ptr, size_t size, size_t nmemb, void *userdata){
     return size * nmemb;
+}
+
+// SynchronizedCharQueueWrapper is a rapidjson adapter which satisfies the
+// `ReadStream` interface for a rapidjson::Reader. This allows us to parse data
+// from curl as we recieve it.
+class SynchronizedCharQueueWrapper {
+  public:
+    typedef char Ch;
+    SynchronizedCharQueueWrapper(SynchronizedCharQueue& stream) : stream_(stream) {}
+    char Peek() const {
+        int c = Front();
+        return c == char_traits<char>::eof() ? '\0' : static_cast<char>(c);
+    }
+    char Take() {
+        int c = Get();
+        return c == char_traits<char>::eof() ? '\0' : static_cast<char>(c);
+    }
+    size_t Tell() const { return (size_t)stream_.size(); }
+    char* PutBegin() { assert(false); return 0; }
+    void Put(char c) { assert(false); }
+    void Flush() { assert(false); }
+    size_t PutEnd(char* c) { assert(false); return 0; }
+  private:
+    int Front() const { return stream_.front(); }
+    int Get() { return stream_.pop(); }
+    SynchronizedCharQueueWrapper(const SynchronizedCharQueueWrapper&);
+    SynchronizedCharQueueWrapper& operator=(const SynchronizedCharQueueWrapper&);
+    SynchronizedCharQueue& stream_;
+};
+
+void write_pretty(UserData& userdata) {
+    // This will make sure to block until we have parsed the header
+    userdata.stream.front();
+
+    boost::string_ref content_type{ userdata.content_type };
+    if (content_type.starts_with("application/json")) {
+        rapidjson::Reader reader;
+        SynchronizedCharQueueWrapper is(userdata.stream);
+        char writeBuffer[65536];
+        rapidjson::FileWriteStream os(stdout, writeBuffer, sizeof(writeBuffer));
+        rapidjson::PrettyWriter<rapidjson::FileWriteStream> writer(os);
+        if (!reader.Parse<rapidjson::kParseValidateEncodingFlag>(is, writer)) {
+            nowide::cerr << "error parsing response" << endl;
+        }
+    } else {
+        int c = userdata.stream.pop();
+        while ( c != char_traits<char>::eof() ) {
+            userdata.error_response += c;
+            c = userdata.stream.pop();
+        }
+    }
 }
 
 void
@@ -128,16 +272,22 @@ pdb_query(const PuppetDBConn& conn,
             + "/pdb/query/v4?query="
             + url_encoded_query.get();
 
+    UserData userdata;
+    userdata.collecting_header = true;
     curl_easy_setopt(curl.get(), CURLOPT_URL, server_url.c_str());
-    curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, stdout);
-    curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &userdata);
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, write_queue);
+    curl_easy_setopt(curl.get(), CURLOPT_HEADER, 1L);
 
+    boost::thread t1(write_pretty, boost::ref(userdata));
     const CURLcode curl_code = curl_easy_perform(curl.get());
+    userdata.stream.push(char_traits<char>::eof());
+    t1.join();
     long http_code = 0;
     curl_easy_getinfo(curl.get(), CURLINFO_RESPONSE_CODE, &http_code);
     if (!(http_code == 200 && curl_code == CURLE_OK)) {
         logging::colorize(nowide::cerr, logging::log_level::fatal);
-        nowide::cerr << "error: " << curl_easy_strerror(curl_code) << endl;
+        nowide::cerr << "error: " << userdata.error_response << endl;
         logging::colorize(nowide::cerr);
     }
 }
