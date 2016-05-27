0231da8e4f94dff4199664c4896a1f3835b19a8e
Andrew Roetker <andrew.roetker@puppetlabs.com>
2016-05-27 16:33:54 -0700

    (PDB-2755) Add acceptance testing to puppetdb-cli
    This commit also aligns our .gitignore with the C++ version of the PDB
    CLI's .gitignore (including some Beaker and Misc files).

---
+++ acceptance/setup/pe/pre_suite/00_setup_env.rb
@@ -0,0 +1,21 @@
+install_opts = options.merge( { :dev_builds_repos => ["PC1"] })
+repo_config_dir = 'tmp/repo_configs'
+
+step "Install Puppet Enterprise." do
+  install_pe
+end
+
+step "Setup pe-client-tools repositories." do
+  install_puppetlabs_dev_repo(master,
+                              'pe-client-tools',
+                              ENV['SHA'],
+                              repo_config_dir,
+                              install_opts)
+end
+
+step "Install pe-client-tools." do
+  host = master
+  install_package(host, 'pe-client-tools')
+  on(host, "/opt/puppetlabs/bin/puppet-query --help")
+  on(host, "/opt/puppetlabs/bin/puppet-query 'nodes{}'")
+end
