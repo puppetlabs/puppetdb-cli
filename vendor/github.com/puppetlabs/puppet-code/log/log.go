01d7508f829667ef296e3cf5e2df66a3c340d9a4
Gabriel Nagy <gabriel.nagy@puppet.com>
2020-07-01 17:37:21 +0300

    (CT-114) Update vendored dependencies

---
+++ vendor/github.com/puppetlabs/puppet-code/log/log.go
@@ -0,0 +1,57 @@
+package log
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/puppetlabs/puppet-code/log/golog"
+	"github.com/puppetlabs/puppet-code/log/loginterface"
+	"github.com/puppetlabs/puppet-code/log/loglevel"
+)
+
+var instance = golog.NewGolog()
+
+//SetInstance sets logger instance
+func SetInstance(log loginterface.Log) {
+	instance = log
+}
+
+//Trace logs a trace message
+func Trace(msg string) {
+	instance.Trace(fmt.Sprintf("TRACE - %s", msg))
+}
+
+//Debug logs a debug message
+func Debug(msg string) {
+	instance.Debug(fmt.Sprintf("DEBUG - %s", msg))
+}
+
+//Info logs an info message
+func Info(msg string) {
+	instance.Info(fmt.Sprintf("INFO - %s", msg))
+}
+
+//Warn logs a warning message
+func Warn(msg string) {
+	instance.Warn(fmt.Sprintf("WARN - %s", msg))
+}
+
+//Error logs an error message
+func Error(msg string) {
+	instance.Error(fmt.Sprintf("ERROR - %s", msg))
+}
+
+//SetLogLevel sets the log level
+func SetLogLevel(logLevel string) error {
+	return instance.SetLogLevel(logLevel)
+}
+
+//GetLogLevel gets the log level
+func GetLogLevel() loglevel.LogLevel {
+	return instance.GetLogLevel()
+}
+
+//setOutput sets log output
+func setOutput(w io.Writer) {
+	instance.SetOutput(w)
+}
---
+++ vendor/github.com/puppetlabs/puppet-code/log/loginterface/log.go
@@ -0,0 +1,19 @@
+package loginterface
+
+import (
+	"io"
+
+	"github.com/puppetlabs/puppet-code/log/loglevel"
+)
+
+//Log interface to logging service
+type Log interface {
+	Trace(msg string)
+	Debug(msg string)
+	Info(msg string)
+	Warn(msg string)
+	Error(msg string)
+	SetOutput(w io.Writer)
+	SetLogLevel(logLevel string) error
+	GetLogLevel() loglevel.LogLevel
+}
